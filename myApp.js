6ee33c510cc80098f7bf67f9aec9593754ee00bb
Glitch (longhaired-quilt-astrodon) <none>
2022-08-20 10:00:17 +0000

    👦👐 Checkpoint ./sample.env:44879746/90 ./package.json:44879746/4320 ./myApp.js:44879746/283

--- myApp.js
+++ myApp.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+  if (err) {
+    console.log(err + "error" + err)
+  } else {
+    console.log("successful database connection");
+  }
+});
+
 
 let Person;
 
